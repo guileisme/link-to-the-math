b72b990a05def8fbdb3024200c8271cbe8490902
Guilherme <guileisme1@gmail.com>
2024-08-27 09:37:38 -0300

    Implementação do NavMesh2D

--- Assets/Scripts/ememy.cs
+++ Assets/Scripts/ememy.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ememy : MonoBehaviour
 {
+    public playerAttack1 player;
     public GameObject enemy;
     public Animator enemyAnimator;
     public BoxCollider2D boxCollider2D;
@@ -14,7 +16,10 @@ public class ememy : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        enemyHitpoints -= 2f;
+        if (player.isAttacking == true)
+        {
+            enemyHitpoints -= 2f;
+        }
     }
 
     void Update()
@@ -22,12 +27,17 @@ public class ememy : MonoBehaviour
         if (enemyHitpoints == 0)
         {
             Die();
+            Disappear();
         }
     }
 
     void Die ()
     {
             enemyAnimator.SetBool("isDead", true);
-            this.enabled = false;
+    }
+
+    void Disappear(){
+            enemy.SetActive(false);
+
     }
 }
--- Assets/Scripts/playerAttack.cs
+++ Assets/Scripts/playerAttack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class playerAttack1 : MonoBehaviour
 {
+    public bool isAttacking; 
     public BoxCollider2D boxCollider2D;
     public Animator animator;
     // Update is called once per frame
@@ -11,12 +12,14 @@ public class playerAttack1 : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.F))
         {
+            isAttacking = true;
             animator.SetBool("isAttacking", true);
             GetComponent<BoxCollider2D>().size = new Vector2(0.3f, 0.3f);
         }
 
         else
         {
+            isAttacking = false;
             animator.SetBool("isAttacking", false);
             GetComponent<BoxCollider2D>().size = new Vector2(0.13f, 0.21f);
         }
